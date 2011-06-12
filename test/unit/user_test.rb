ecb273c6fbe861d329655279c37bf37e7be59188
Régis Behmo <regisb@gmail.com>
2011-06-12 18:19:13 +0200

    Removed a bunch of useless tests, added a couple new one. Email validation doesn't work. Weird.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-
   test "User with valid email and password" do
     u = User.new
     u.email = "dude2@corp.com"
@@ -16,38 +11,24 @@ class UserTest < ActiveSupport::TestCase
     assert(u.valid?)
   end
 
-
-  test "has email" do
-    u = users(:got_email)
-    assert_not_nil(u.email)
-    assert_not_equal("", u.email)
-  end
-  
-  test "user without email is invalid" do
-    u = users(:got_no_email)
-    assert_equal(false, u.valid?)
+  test "User with invalid email" do
+    u = User.new
+    u.email = "dude"
+    u.password = "1234"
+    assert(!u.save, "User with email #{u.email} should not be valid")
   end
 
-  test "user with empty email is invalid" do
-    u = users(:got_empty_email)
-    assert_equal(false, u.valid?)
+  test "user with duplicate email" do
+    u1 = users(:got_email)
+    u2 = User.new
+    u2.email = u1.email
+    u2.password = "123456789"
+    assert(!u2.valid?, "User with duplicate email should not be valid")
   end
 
-  test "user with valid email" do
-    u = users(:got_email)
-    assert_equal(true, u.valid?)
-  end
-  
-  test "user with invalid email" do
-    u = users(:got_invalid_email)
-    assert_equal(false, u.valid?, "User is considered valid while his email is #{u.email}")
-  end
-
-  test "users with duplicate emails" do
-    u = users(:got_email)
-    u.save
-    v = User.new
-    v.email = u.email
-    assert(!v.save)
+  test "User with valid email and no password" do
+    u = User.new
+    u.email = "dude@corp.com"
+    assert(!u.valid?, "user with empty password should not be valid")
   end
 end
