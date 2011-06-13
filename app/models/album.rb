d009080a901bfb684cc35a3b72572d49d0c0f7dc
Régis B. @ Bluemountain <regis.behmo@gmail.com>
2011-06-13 16:20:55 +0200

    Added secret to album

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,16 @@
 class Album < ActiveRecord::Base
+  validates_presence_of :title
+  belongs_to :user
+  has_many :images
+
+  validates_presence_of :secret
+  validates_length_of :secret, :is => 32
+  validates_uniqueness_of :secret
+  validate :create_secret, :on => :create
+
+  private
+  def create_secret
+    allowed = [('a'..'z'),('0'..'9')].map{|i| i.to_a}.flatten
+    self.secret = (0..50).map{ allowed[rand(allowed.length)]  }.join
+  end
 end
