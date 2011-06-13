9c02aa667b74825d8884f7233c058e0833595588
Régis B. @ Bluemountain <regis.behmo@gmail.com>
2011-06-13 18:54:38 +0200

    Fixed routes (not very pretty)

--- config/routes.rb
+++ config/routes.rb
@@ -55,5 +55,5 @@ RailsSandbox::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   match ':controller(/:action(/:id(.:format)))'
-  match 'album/:secret' => 'album#index'
+  match 'show/:secret' => 'album#index'
 end
