b3daa831d31f0bb9b1ded274b53eac1353439197
Régis Behmo <regisb@gmail.com>
2011-07-10 22:57:41 +0200

    Switched thumb size to 245x245

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :img, :styles => {:medium => "1024x1024>", :thumb => "100x100>"}
+  has_attached_file :img, :styles => {:medium => "1024x1024>", :thumb => "245x245>"}
   validates_attachment_presence :img
   after_save :add_exif_tags
   belongs_to :album
